aef46329ddd520afea02b30dba67b893f4e8499e
12345koip <danbarnes2019@hotmail.com>
2026-06-22 12:26:49 +0100

    fix waypoint definition as of rn

--- src/Waypoints/Definitions.cpp
+++ src/Waypoints/Definitions.cpp
@@ -17,4 +17,21 @@ void Waypoint::SetEnabled(bool newEnabled) {
 
 void Waypoint::SetGlobal(bool newGlobal) {
     this->m_bIsGlobal = newGlobal;
+
+    if (newGlobal)
+        this->m_levelID = 0; //if it was a mistake, the user can click the button again and it'll grab the level ID
+}
+
+void Waypoint::SetLevelID(uint64_t newLevelId) {
+    this->m_levelID = newLevelId;
+}
+
+bool Waypoint::operator==(const std::shared_ptr<Waypoint> other) const {
+    return (
+        this->m_levelID == other->GetLevelID() &&
+        this->m_bIsGlobal == other->IsGlobal() &&
+        this->m_bEnabled == other->IsEnabled() &&
+        this->m_activationPercentage == other->GetTriggerPercentage() &&
+        this->m_behaviourType == other->GetBehaviourType()
+    );
 }
--- src/Waypoints/Definitions.hpp
+++ src/Waypoints/Definitions.hpp
@@ -14,20 +14,33 @@ namespace AutoPauseMod::Waypoints {
 
     class Waypoint final {
         private:
+            uint64_t m_levelID; //TODO: check it's not uint32_t
             WaypointBehaviourType m_behaviourType;
             float m_activationPercentage; //TODO: check gd actually uses a float or other floating-point thing for this umm can change if not sob
             bool m_bEnabled;
             bool m_bIsGlobal;
 
+
         public:
-            explicit Waypoint(WaypointBehaviourType type, float percentage = 0):
-                m_behaviourType(type), m_activationPercentage(percentage), m_bEnabled(true), m_bIsGlobal(false) {};
+            explicit Waypoint(WaypointBehaviourType type, float percentage = 0, uint32_t levelID = 0):
+                m_behaviourType(type), m_activationPercentage(percentage), m_bEnabled(true), m_bIsGlobal(false), m_levelID(levelID) {
+                    //if the given level ID is 0 and isGlobal is false, we will quite simply
+                    //just assume the level ID could not be found or else a problem arose
+                    //and just "lock" it as global for safety.
+                    if (levelID == 0 && !this->m_bIsGlobal)
+                        this->m_bIsGlobal = true;
+                };
             ~Waypoint() = default;
             Waypoint(const Waypoint&) = delete;
             Waypoint& operator=(const Waypoint&) = delete;
             Waypoint(Waypoint&&) = delete;
 
 
+            void SetBehaviourType(WaypointBehaviourType newType);
+            WaypointBehaviourType GetBehaviourType() const {return this->m_behaviourType;}
+
+            void SetLevelID(uint64_t newLevelId);
+            uint64_t GetLevelID() const {return this->m_levelID;}
 
             void SetTriggerPercentage(float newPercentage);
             uint8_t GetTriggerPercentage() const {return this->m_activationPercentage;}
@@ -39,6 +52,6 @@ namespace AutoPauseMod::Waypoints {
             void SetGlobal(bool newGlobal);
 
             bool ShouldPause(float currentPercentage) const;
-            bool operator==(const Waypoint& other) const;
+            bool operator==(const std::shared_ptr<Waypoint> other) const;
     };
 }
